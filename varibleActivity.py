e8cbc895e4b012c89f0747c5d3c3b8af45ba14a5
neem827 <nkelly832@boyslatin.net>
2026-09-03 13:45:32 +0000

    work

---
+++ varibleActivity.py
@@ -0,0 +1,44 @@
+#variables- a contaier that stores data.
+#you can name your contaier almost anything you want, so long 
+#as it is descriptive.
+
+name= "nehemiah kelly"
+grade=10 
+
+
+# variable rules 
+1.A variable must start with a letter and cannot start with number 
+2.A variable can be a mixd of letters and numbers but not symbols
+3.A variable cannot be python keyword
+
+
+numberofapples= 20
+numberofmathstudents=26
+aadtripinsurance=100
+goldgymmembership=1
+silvergymmembership=2
+priceofdiffentdrink=48
+
+
+
+camal case- when creating variable names with multipble words,
+evrey new word begins with captal letter 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
