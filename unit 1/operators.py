e7f6128db8afe21ab27cad290dd148ec59089285
neem827 <nkelly832@boyslatin.net>
2026-09-10 13:18:10 +0000

    warmup

---
+++ unit 1/operators.py
@@ -0,0 +1,24 @@
+#asigment operators-Exclusively used to
+#assign values to variables.
+#think key / value pairings
+
+# we use a single equal sign to represnt the assingment 
+#operator
+name=nehemiah
+grade=10
+school=True
+
+#Arithmetic operators-used on numerical 
+# data types to perform calculation.
+#intergers (whole numbers )and floats (decimal numbers)
+
+#print is a function that lets us show code 
+#in termainal
+print(3+3)# addition operator
+print(3-3)# subtraction operator
+print(12/3) # divsion operator
+print(3*4) # multiplacation operator
+
+# comparison operators- set of symbols used 
+# to assess if data is the same or different and
+#how they differ
