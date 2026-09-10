148e358710feb897b26225f09d8680c03cc836c2
neem827 <nkelly832@boyslatin.net>
2026-09-10 13:42:25 +0000

    notes

--- unit 1/operators.py
+++ unit 1/operators.py
@@ -22,3 +22,34 @@ print(3*4) # multiplacation operator
 # comparison operators- set of symbols used 
 # to assess if data is the same or different and
 #how they differ
+
+print(10.1) #greater than operator
+print(2000<100) #less than operator
+
+#2 equal signs compare if something is the same
+print("book"=="Book") # same as (false)
+print(2.0==2) # same as (true)
+
+
+#not equal is written with !=
+#this is to check and filter for values that are not
+#the same
+print(200 != 100) #true- these are not the same
+print(300 != 300)# false- these are not the ame
+
+
+#logical operators- compares 2 conditions to check if
+#they are true or false
+
+# And - checks if 2 conitions are true. if yes the final
+#result is ture 
+print(3 > 1 and 100 > 50) # this would come out to be true 
+
+#OR- checks if only 1 condition is ture. if yes,
+# the final result will be true
+print (3 < 1 or 100 > 50) 
+
+
+#NOT - the "oppostie day" operator. it will revers the
+#result of logical operators
+print(3 > 1 or 100 > 50) 
