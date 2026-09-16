a6e1bf4636d709556fab9f045a861c0c6f0272d0
neem827 <nkelly832@boyslatin.net>
2026-09-16 13:45:03 +0000

    ture or falsee

--- unit 1/operatorsActivity.py
+++ unit 1/operatorsActivity.py
@@ -1,10 +1,29 @@
+from contextlib import closing
+
+
 print(200 == 100) 
 #false
 print(15 == 18)
 #false
-print ( coding 1 == coding 1)
+print (closing 1 == coding 1)
 #false
 print (0 == 0)
 #true
+
+Book=10.99
+tablet=399.99
+print(Book + tablet)
+
+cart=98.87
+amouttogetdiscount=100.00
+print(cart + amouttogetdiscount)
+
+shoes=200.00
+Tax=0.07
+print(shoes + Tax)
+
+gpa=86
+recommedaton=False
+print(gpa > 85 recommedaton == true)
 
 
