2594094d7969c86a46dee746ea5367489e95c47c
neem827 <nkelly832@boyslatin.net>
2026-09-23 13:48:27 +0000

    new notes

---
+++ unit2/builtinfunctions.py
@@ -0,0 +1,23 @@
+#functions - simply put ; a code block of
+#instructions for comuputers to follow.
+
+#built in functions - a code block 
+#instructions for a comuputer to follow
+#that was already written for us
+#PRE WRITTEN CODE INSTRUCTIONS
+
+#input()a built in function that allows a user to
+#type in data into termial . the data that is passed
+#in all will always be treated as a string data type
+
+#print() a bulit in function that allows a user to
+#print data out in the turminaml
+
+
+
+#float- A function that will change any datatype
+#passed into it, into float/decimal number
+
+num2 = input ("type in a number")
+#input always returns a string 
+#print(9 + float(num9)
---
+++ unit2/functionActivity.py
@@ -0,0 +1,10 @@
+#1
+
+val= input ("please type in a number:")
+print(int(val)>85)
+
+#notes: 
+# need to use input to take in a number
+#compring the number we type in to 85
+#print true if the number is over 85
+#print false if number is under 85
